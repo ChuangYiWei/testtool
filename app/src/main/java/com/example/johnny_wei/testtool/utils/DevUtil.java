39dc24bd51793a342a2737f6c6fda344e8d389bf
johnny <fireiori70807@gmail.com>
2019-03-11 17:49:31 +0800

    1. add check feature.

---
+++ app/src/main/java/com/example/johnny_wei/testtool/utils/DevUtil.java
@@ -0,0 +1,44 @@
+package com.example.johnny_wei.testtool.utils;
+
+import android.content.Context;
+import android.os.Build;
+import android.util.Log;
+
+import static android.content.pm.PackageManager.FEATURE_BLUETOOTH;
+import static android.content.pm.PackageManager.FEATURE_BLUETOOTH_LE;
+
+public class DevUtil {
+    private static final String TAG = "DevUtil";
+
+    public static String GetDeviceModel(){
+        return Build.MODEL;
+    }
+
+    public static String GetSystemVersion(){
+        return Build.VERSION.RELEASE;
+    }
+
+    public static String GetBrand(){
+        return Build.BRAND;
+    }
+
+    public static int GetVersionSDK(){
+        return Build.VERSION.SDK_INT;
+    }
+
+    public static void printDeviceInfo() {
+        Log.d(TAG, "Brand:" + DevUtil.GetBrand());
+        Log.d(TAG, "Dev model:" + DevUtil.GetDeviceModel());
+        Log.d(TAG, "    System ver:" + DevUtil.GetSystemVersion());
+        Log.d(TAG, "    SDK ver:" + DevUtil.GetVersionSDK());
+    }
+
+    public static boolean hasBLUETOOTHFeature(Context argContext) {
+        return (argContext == null) ? false : argContext.getPackageManager().hasSystemFeature(FEATURE_BLUETOOTH);
+    }
+
+    public static boolean hasBLEFeature(Context argContext) {
+        return (argContext == null) ? false : argContext.getPackageManager().hasSystemFeature(FEATURE_BLUETOOTH_LE);
+    }
+
+}
