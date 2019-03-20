0972e8c9922df7845190fec27231f3441be48c9d
johnny <fireiori70807@gmail.com>
2019-03-20 10:27:51 +0800

    1. add expandable view example code

---
+++ app/src/main/java/com/example/johnny_wei/testtool/adapter/Group.java
@@ -0,0 +1,21 @@
+package com.example.johnny_wei.testtool.adapter;
+
+
+public class Group {
+    private String gName;
+
+    public Group() {
+    }
+
+    public Group(String gName) {
+        this.gName = gName;
+    }
+
+    public String getgName() {
+        return gName;
+    }
+
+    public void setgName(String gName) {
+        this.gName = gName;
+    }
+}
