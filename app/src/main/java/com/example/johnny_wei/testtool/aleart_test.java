857d9dccb28b3305ff36e54199143514808ccd2b
johnny <fireiori70807@gmail.com>
2019-10-18 12:46:29 +0800

    add radio button in alert dialog

--- app/src/main/java/com/example/johnny_wei/testtool/aleart_test.java
+++ app/src/main/java/com/example/johnny_wei/testtool/aleart_test.java
@@ -10,22 +10,55 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class aleart_test extends AppCompatActivity {
     private final String TAG = getClass().getSimpleName();
     EditText txt;
     LayoutInflater inflater;
     AlertDialog.Builder builder;
-
+     View dialogView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aleart_test);
+
+        //quick setup
+        //CreateAlertDialogWithRadioButtonGroup();
+
+
     }
 
+    RadioButton rdx_ecg;
+    RadioButton rdx_ppg;
+    private void radio_setup() {
+        rdx_ecg = findViewById(R.id.radio_ecg);
+        rdx_ppg = findViewById(R.id.radio_ppg);
+        RadioGroup radioGroup_tx_rx = dialogView.findViewById(R.id.radio_ppg_ecg);
+        radioGroup_tx_rx.setOnCheckedChangeListener(mOnCheckedChangeListener);
+    }
+    private RadioGroup.OnCheckedChangeListener mOnCheckedChangeListener = new RadioGroup.OnCheckedChangeListener() {
+
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            switch (checkedId) {
+                case R.id.radio_ecg:
+                    Toast.makeText(aleart_test.this, "ECG select", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.radio_ppg:
+                    Toast.makeText(aleart_test.this, "PPG select", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+
     public void clk_start(View view) {
+        //custom view
+//custom alert
         inflater = this.getLayoutInflater();
-        final View dialogView = inflater.inflate(R.layout.test_aleart, null);
+        dialogView = inflater.inflate(R.layout.test_aleart, null);
         builder = new AlertDialog.Builder(this);
         builder.setTitle("這是標題")
                 .setView(dialogView)
@@ -33,41 +66,37 @@ public class aleart_test extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         //do something
-                        txt = dialogView.findViewById(R.id.edt_name_alert);
-                        Log.d(TAG,"name in alert is:" + txt.getText().toString());
                     }
                 });
         AlertDialog dialog = builder.create();
 //        AlertDialog dialog = (AlertDialog) createAddPersonDialog();
         dialog.show();
-
-
+        radio_setup();
     }
-    private Dialog createAddPersonDialog() {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(aleart_test.this);
 
-        // Inflate using dialog themed context.
-        final Context context = builder.getContext();
-        final LayoutInflater inflater = LayoutInflater.from(context);
-        final View view = inflater.inflate(R.layout.activity_aleart_test, null, false);
-
-        // Find widgets inside "view".
-        final EditText name = (EditText) view.findViewById(R.id.edt_name_alert);
-
-
-        final View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String user_name = (String) name.getText().toString();
-                Log.d(TAG,"name in alert is:" + user_name);
+    AlertDialog alertDialog1;
+    CharSequence[] values = {" First Item "," Second Item "," Third Item "};
+    public void CreateAlertDialogWithRadioButtonGroup(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(aleart_test.this);
+        builder.setTitle("Select Your Choice");
+        builder.setSingleChoiceItems(values, -1, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+                switch (item) {
+                    case 0:
+                        Toast.makeText(aleart_test.this, "First Item Clicked", Toast.LENGTH_LONG).show();
+                        break;
+                    case 1:
+                        Toast.makeText(aleart_test.this, "Second Item Clicked", Toast.LENGTH_LONG).show();
+                        break;
+                    case 2:
+                        Toast.makeText(aleart_test.this, "Third Item Clicked", Toast.LENGTH_LONG).show();
+                        break;
+                }
+                alertDialog1.dismiss();
             }
-        };
-
-        builder
-                .setView(view)
-                .setCancelable(false);
-//                .setPositiveButton("OK", listener)
-//                .setNegativeButton("Cancel", listener);
-        return builder.create();
+        });
+        alertDialog1 = builder.create();
+        alertDialog1.show();
     }
+
 }
