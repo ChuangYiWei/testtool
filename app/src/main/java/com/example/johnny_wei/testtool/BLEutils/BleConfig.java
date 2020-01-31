3975dcb5e369bb103d85a91266c7841989bd0e2d
johnny <fireiori70807@gmail.com>
2020-01-31 14:11:15 +0800

    update ble util form Bluetooth_LE

---
+++ app/src/main/java/com/example/johnny_wei/testtool/BLEutils/BleConfig.java
@@ -0,0 +1,21 @@
+package com.ble.johnny.Bluetooth_LE.BLEutils;
+
+public class BleConfig {
+    private static BleConfig instance;
+    private int DEFAULT_SCAN_TIME = 5000;
+    private int DEFAULT_RETRY_COUNT = 3;
+
+    private BleConfig() {
+    }
+
+    public static BleConfig getInstance() {
+        if (instance == null) {
+            synchronized (BleConfig.class) {
+                if (instance == null) {
+                    instance = new BleConfig();
+                }
+            }
+        }
+        return instance;
+    }
+}
---
+++ app/src/main/java/com/example/johnny_wei/testtool/BLEutils/BleExceptionCode.java
@@ -0,0 +1,10 @@
+package com.ble.johnny.Bluetooth_LE.BLEutils;
+
+public class BleExceptionCode {
+    //GATT error code
+    public static  int GATT_CONN_TERMINATE_LOCAL_HOST = 0x16;
+    public static  int GATT_CONN_TIMEOUT = 0x08;
+    public static  int GATT_INTERNAL_ERROR = 0x81;//129
+
+
+}
